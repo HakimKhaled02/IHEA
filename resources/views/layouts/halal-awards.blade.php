203c276a4ebfd29bf9c21d735990c523b646375f
ikram03-alif <ikramalif.roslee.com>
2025-12-17 14:02:38 +0800

    Update halal-awards.blade.php to use Montserrat font

--- resources/views/layouts/halal-awards.blade.php
+++ resources/views/layouts/halal-awards.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'International Halal Economic Awards 2026')</title>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     @stack('styles')
 </head>
